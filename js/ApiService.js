787a63b716a9c4299db8c21eca2c2346e3050c17
Vasyl Poklitar <vasiok26@gmail.com>
2017-02-25 22:50:57 +0200

    te

--- js/ApiService.js
+++ js/ApiService.js
@@ -1,32 +1,38 @@
-﻿(function (app) {
-	'use strict';
+﻿console.log("in script");
+function gg() {
+	console.log("in script2");
 
+	mailru.loader.require('api', function () {
+
+		mailru.connect.init('752318', '83de107fba691d4967a109240fa0d56b');
+
+		mailru.events.listen(mailru.connect.events.login, function (session) {
+			window.location.reload();
+		});
+		mailru.events.listen(mailru.connect.events.logout, function () {
+			window.location.reload();
+		});
+
+		mailru.connect.getLoginStatus(function (result) {
+			if (result.is_app_user != 1) {
+				console.log("no connect To user");
+			} else {
+				mailru.common.photos.get(function (photos_list) {
+					console.log(photos_list);
+				}, 1);
+				mailru.common.users.getInfo(function (result) { console.log(result[0]) });
+			}
+		});
+	});
+}
+(function (app) {
+	'use strict';
+	console.log("in App");
 	app.controller('ApiConnect', ApiConnect);
 	function ApiConnect($rootScope){
-	
-		mailru.loader.require('api', function () {
+		gg();
+		console.log("after gg");
 
-			mailru.connect.init('752318', '83de107fba691d4967a109240fa0d56b');
-
-			mailru.events.listen(mailru.connect.events.login, function (session) {
-				window.location.reload();
-			});
-			mailru.events.listen(mailru.connect.events.logout, function () {
-				window.location.reload();
-			});
-
-			mailru.connect.getLoginStatus(function (result) {
-				if (result.is_app_user != 1) {
-					console.log("no connect To user");
-				} else {
-					mailru.common.photos.get(function (photos_list) {
-						console.log(photos_list);
-					}, 1);
-					mailru.common.users.getInfo(function (result) { console.log(result[0]) });
-				}
-			});
-		});
-		//]]>
 	}
 
 })(angular.module('TextBookApp'));
