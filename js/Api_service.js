315449ee5aa2189703d00f01907178a2e97dd2ae
Vasyl Poklitar <vasiok26@gmail.com>
2017-02-25 20:58:57 +0200

    tex

---
+++ js/Api_service.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+	'use strict';
+	console.log("in service");
+mailru.loader.require('api', function () {
+	mailru.connect.init('752318','83de107fba691d4967a109240fa0d56b');
+	mailru.common.photos.get(function (photos_list) {
+		// выведет название первой фотографии из альбома с aid 123
+		console.log(photos_list);
+	}, 1);
+	console.log("in connect");
+
+
+});
+
+})(angular.module('TextBookApp'));
